56d1e25059fc518660c6bcafea02e000ab2373e2
ClaraAravecchia <51388856+ClaraAravecchia@users.noreply.github.com>
2021-09-30 23:47:53 -0300

    Adição do grafico de variância

--- variancia_desviopadrao.py
+++ variancia_desviopadrao.py
@@ -1,6 +1,7 @@
 import math
 import matplotlib.pyplot as plt
 
+
 def XPx(x,px):
 	soma = 0
 	sum = 0
@@ -34,13 +35,22 @@ def desvio(media, dp):
 	print("Desvio 1 - 68%%   - Min %.2f Max %.2f" %(min, max))
 	print("Desvio 2 - 95%%   - Min %.2f Max %.2f" %(min2, max2))
 	print("Desvio 3 - 99.7%% - Min %.2f Max %.2f" %(min3, max3))
+	return (min, max, min2, max2, min3, max3)
 
-def graph(x, px, media, var, dp):
-	plt.title("Media - %.2f   VAR - %.2f   DP - %.2f" %(media, var, dp))
+def graph(x, px, media, var, dp, desvio):
+	plt.figure(figsize=(8, 6))
+	plt.title("[Media  %.2f]   [VAR  %.2f]   [DP  %.2f]" %(media, var, dp))
 	plt.xlabel("X")
 	plt.ylabel("P(x)")
+	plt.grid(True)
 
 	plt.plot(x, px, C = 'purple', ls='-', lw='1', marker='o')
+	m = sum(px) / float(len(px))
+ 
+	plt.plot(desvio[0:2], [m+(m/2), m+(m/2)], ls='--', color = '#10A0DF', label="68%% Min %.2f Max %.2f"%(desvio[0], desvio[1]))
+	plt.plot(desvio[2:4], [m, m], ls='--', color = 'red',  label="95%% Min %.2f Max %.2f"%(desvio[2], desvio[3]))
+	plt.plot(desvio[4:6], [m/2, m/2], ls='--', color = '#FFB31D', label="99.7%% Min %.2f Max %.2f"%(desvio[4], desvio[5]))
+	plt.legend()
 	plt.show()
 
 
@@ -54,6 +64,6 @@ print("Media - %.2f" %media)
 print("VAR - %.2f" %var)
 print("DP - %.2f" %dp)
 
-desvio(media, dp)
+d = desvio(media, dp)
 # Descomentar para visualizar o grafico
-#graph(x, px, media, var, dp)
+graph(x, px, media, var, dp, d)
